0c29c4ef8e0e0b162c5fc3dfe95d484c76b177f6
Siegfried Ehret <siegfried@ehret.me>
2024-06-08 07:43:07 +0200

    🐉 clean some stuff

--- src/pages/random/index.ts
+++ src/pages/random/index.ts
@@ -1,7 +1,7 @@
 import type { APIRoute } from "astro";
 import { getRandomRow } from "../../lib/database.ts";
 
-export const GET: APIRoute = async ({ params, redirect }) => {
+export const GET: APIRoute = async ({ redirect }) => {
   const entries = await getRandomRow();
 
   return redirect(entries[0].domain);
